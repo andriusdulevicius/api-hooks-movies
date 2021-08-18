47f7551c9b851c2aa2a5864c4eaded507dd1b889
AndriusDulevicius <andriusdulevicius@email.com>
2021-08-18 12:05:09 +0300

    try catch , error case handling

--- src/App.jsx
+++ src/App.jsx
@@ -20,19 +20,25 @@ function App() {
   ];
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMovieshandler() {
     setIsLoading(true);
-    const { data } = await axios.get('https://swapi.dev/api/films');
-    console.log(data.results);
-    // perdaryti gautus duomenis pagal savo formata
-    const moviesTransformed = data.results.map((mv) => ({
-      id: mv.episode_id,
-      title: mv.title,
-      openingText: mv.opening_crawl,
-      releaseDate: mv.releaseDate,
-    }));
-    setMovies(moviesTransformed);
+    try {
+      const { data } = await axios.get('https://swapi.dev/api/film');
+      console.log(data.results);
+      // perdaryti gautus duomenis pagal savo formata
+      const moviesTransformed = data.results.map((mv) => ({
+        id: mv.episode_id,
+        title: mv.title,
+        openingText: mv.opening_crawl,
+        releaseDate: mv.releaseDate,
+      }));
+      setMovies(moviesTransformed);
+    } catch (err) {
+      console.log(err.message);
+      setError(true);
+    }
     setIsLoading(false);
   }
 
@@ -46,6 +52,7 @@ function App() {
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
         {!isLoading && movies.length === 0 && <p>No movies at the moment</p>}
+        {error && <p>Ivyko klaida</p>}
       </section>
     </React.Fragment>
   );
